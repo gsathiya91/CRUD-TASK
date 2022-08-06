64ae779b202a967359da413b889cb1ff887c52a2
Sathiya priya <97939755+gsathiya91@users.noreply.github.com>
2022-08-06 16:03:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const cors = require('cors');
 const PORT = process.env.PORT || 5000
 
 //cors
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+}));
 //cookie-parser
 app.use(cookieParser());
 
